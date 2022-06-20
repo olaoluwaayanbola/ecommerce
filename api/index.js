d052daca2a367c984b860efe421e221973ad0ef4
Ayanbola <olaoluwaayanbola@gmail.com>
2022-06-20 22:37:31 +0100

    fixed the login authentication bug

--- api/index.js
+++ api/index.js
@@ -3,11 +3,14 @@ const app = express();
 const mongoose = require("mongoose")
 const dotenv = require('dotenv');
 const authRoute = require("./routes/auth");
+
 dotenv.config()
 
 mongoose.connect(process.env.MONGODB_URL)
-.then(()=>console.log("DB CONNECTED"))
-.catch((err)=>console.log(`DB CONNECT ${err}`))
+    .then(() => console.log("DB CONNECTED"))
+    .catch((err) => console.log(`DB CONNECT ${err}`))
+
+app.listen(8180);
 
 app.use(express.json());
 app.use("/api/auth", authRoute);
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const router = express.Router();
-
+const router = require("express").Router();
 const User = require("../model/User");
-
+const CryptoJS = require("crypto-js");
+const { json } = require("express");
+//REGISTER
+//create user with body and encrypt password with crypto-js
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password:req.body.password,
-});
+    password: CryptoJS.AES.encrypt(
+      req.body.password, "laolu"
+    ).toString(),
+  });
   try {
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
@@ -16,4 +19,35 @@ router.post("/register", async (req, res) => {
     res.status(500).json(err);
   }
 });
-module.exports = router ;
+
+// LOGIN-INFORMATION
+// User will login using name and passwod
+
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne(
+      {
+        username: req.body.username
+      }
+    );
+
+    !user && res.status(401).json("Wrong User Name");
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      "laolu"
+    ).toString(CryptoJS.enc.Utf8)
+
+    console.log(hashedPassword );
+    console.log(req.body.password)
+
+    if( hashedPassword !== req.body.password ){
+      response.status(401).json("Wrong passwords");
+    }
+    
+    res.status(200).json(user)
+  }catch(err){
+    res.status(500).json(err);
+}
+});
+module.exports = router;
