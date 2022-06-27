98750f68bb4d4cc55441d3047503406358771a70
Ayanbola <olaoluwaayanbola@gmail.com>
2022-06-27 06:09:16 +0100

    --products api

--- client/src/Components/Products/Product.jsx
+++ client/src/Components/Products/Product.jsx
@@ -7,6 +7,17 @@ const Product = ({ Category, filter, Sort }) => {
   const [Productdata, setProductdata] = useState([]);
   const [filteredProducts, setFiltersProducts] = useState({});
 
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get("http://localhost:3000/api/products");
+        console.log(res)
+      } catch (err) {
+        console.log(err +"cowww")
+      }
+    }
+    getProducts();
+  }, [Category]);
   
   return (
     <div className="Products-Container-m">
