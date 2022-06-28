e29c96565f4cf6677a592b8b6796576df4976dac
Ayanbola <olaoluwaayanbola@gmail.com>
2022-06-28 01:16:47 +0100

    firebase authentication

--- client/src/Pages/Signin/Signin.jsx
+++ client/src/Pages/Signin/Signin.jsx
@@ -1,22 +1,100 @@
 import React from "react";
 import "./Signin.scss";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import { useState } from "react";
+
 const Signin = () => {
+  const [info, setInfo] = useState({
+    Name: "",
+    lastName: "",
+    userName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  console.log(info);
+  const handleForm = (event) => {
+    const value = event.target.value;
+    setInfo({
+      ...info,
+      [event.target.name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(info.email)
+    console.log(info.userName)
+    createUserWithEmailAndPassword(auth, info.email, info.password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+        // ..
+      });
+  };
   return (
     <div className="Sign-in-container">
       <div className="Wrapper">
         <h3>CREATE AN ACCOUNT</h3>
-        <form action="">
-          <input type="text" placeholder="name" />
-          <input type="text" placeholder="lastname" />
-          <input type="text" placeholder="username" />
-          <input type="email" placeholder="email" />
-          <input type="password" placeholder="Password" />
-          <input type="password" placeholder="confirm password" />
+        <form action="" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="name"
+            name="Name"
+            onChange={handleForm}
+            value={info.Name}
+          />
+          <input
+            type="text"
+            placeholder="lastname"
+            name="lastName"
+            onChange={handleForm}
+            value={info.lastName}
+          />
+          <input
+            type="text"
+            placeholder="username"
+            name="userName"
+            onChange={handleForm}
+            value={info.userName}
+          />
+          <input
+            type="email"
+            placeholder="email"
+            name="email"
+            onChange={handleForm}
+            value={info.email}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={handleForm}
+            value={info.password}
+          />
+          <input
+            type="password"
+            placeholder="confirm password"
+            name="confirmPassword"
+            onChange={handleForm}
+            value={info.confirmPassword}
+          />
         </form>
         <div className="policy">
           lorenm ipsum adipisci facilis expedita veritatis unde excepturi.
         </div>
-        <button>CREATE USER</button>
+        <button type="submit" onClick={handleSubmit}>
+          CREATE USER
+        </button>
       </div>
     </div>
   );
